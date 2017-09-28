053cacbae56f040edf605b55a94f0caa6ecad8fd
christianchiarulli <chrisatmachine@gmail.com>
2017-09-27 21:46:00 -0400

    starting project

--- test.asm
+++ test.asm
@@ -1,14 +1,16 @@
 #Program 1 - Takes a string and reverses the string
 #
 # 
-# Cosc 300
+# takes variables starting with either upper or lower case letter
 
 .data
 
 prompt: .asciiz ">> "
 newline: .asciiz "\n"
-invalid: .asciiz " invalid input " 
-
+invalid: .asciiz " invalid input "
+notvarms: .asciiz " not var" 
+madeAVar: .asciiz "made a var"
+iamhere: .asciiz "here i am"
 inputString: .space 64
 
 
@@ -17,29 +19,42 @@ inputString: .space 64
 
 main:
 la      $a0, prompt     #calling opening prompt
-li      $v0, 4
+li      $v0, 4		# load 4
 syscall
 
 # ==============================================================================
 
-la $a0, inputString
-li $a1 65
-li $v0, 8
+la $a0, inputString	#load address with my allocated space for string
+li $a1 65		# allocate space
+li $v0, 8		# user input string
 syscall
 
-la      $a0, newline    #newline
-syscall
+#la      $a0, newline    #newline
 
-#la      $a0, ans        #initial text for reversed string
-#syscall
+syscall
 
 li      $t2, 0
 
-loop:
+
+#bgt $t2, 0, notvar # so it only checks the first character
+lb $s0, inputString($t2) # load first char into register
+#add $t2, $t2, 1		# iterate to second char
+syscall
+bgeu  $s0, 'A', checkIfLessThanZ # checks caps
+
+j notvar # not a var if not between A-Z
+
+
+# declares a variable
+declareVar:
+
 lb $t0, inputString($t2)
-beq $t2, ')', inv
 add $t2, $t2, 1
-j loop
+beq $t0, ' ', endOfVarName
+beq $t0, $zero, main # finds end of string
+add $t2, $t2, 1
+
+j declareVar
 	
 inv:
 
@@ -52,19 +67,82 @@ syscall
 
 j main
 
-#strLen:                 #getting length of string
-#lb      $t0, inputString($t2)   #loading value
-#add     $t2, $t2, 1
-#bne     $t0, $zero, strLen
-#sub     $t2, $t2, 1
-#li      $v0, 11         #load imediate - print low-level byte
+notvar:
 
-#Loop:
-#la      $t0, str($t2)   #loading value
-#lb      $a0, ($t0)
-#syscall
-#sub     $t2, $t2, 1
-#bnez    $t2, Loop
+la      $a0, notvarms   #calling opening prompt
+li      $v0, 4
+syscall
 
-#li $v0, 10              #program done: terminating
-#syscall
+la      $a0, newline    #newline
+syscall
+
+
+j main
+
+
+#################################### variable declaration #####################################
+checkIfLessThanZ:
+bleu  $t0, 'Z', declareVar # checks caps
+j lowercase
+CheckIfLessThanz:
+bleu  $t0, 'z', declareVar # checks caps
+j inv # this is where I stoppped
+lowercase:
+bgeu  $t0, 'a', CheckIfLessThanz # checks caps
+j notvar
+
+
+endOfVarName:
+add $t2, $t2, 1
+lb $t0, inputString($t2)
+beq $t0, ' ', endOfVarName # keep loading spaces until you see = 
+beq $t0, '=', FindNumber
+
+j hereiam
+
+FindNumber:
+add $t2, $t2, 1
+lb $t0, inputString($t2)
+beq $t0, ' ', FindNumber # keep loading spaces until you see a number
+beq $t0, '+', FindNumber # keep loading + until you see a number
+beq $t0, '-', FindNumber # keep loading - until you see a number
+bgeu $t0, 0, CheckLessThanNine
+
+j inv
+
+CheckGreaterThanZero:
+bleu $t0, 0, CheckLessThanNine #ready to find the number
+j inv
+
+CheckLessThanNine:
+bleu $t0, 9, keepGoing #ready to find the number
+j inv
+
+keepGoing:
+#GOTOstore
+
+
+la      $a0, madeAVar   #calling opening prompt
+li      $v0, 4
+syscall
+
+la      $a0, newline    #newline
+syscall
+
+beq $t0, $zero, main # finds end of string
+
+
+
+
+#################################### variable declaration #####################################
+
+hereiam:
+
+la      $a0, iamhere  #calling opening prompt
+li      $v0, 4
+syscall
+
+la      $a0, newline    #newline
+syscall
+
+j main
---
+++ tryaggain.asm
@@ -0,0 +1,137 @@
+
+.data
+stringstore: .space 2000
+numstore: .space 2000
+prompt: .asciiz ">> "
+str: .space 64
+newline: .asciiz "\n"
+madeit: .asciiz "made it"
+nv: .asciiz "not a variable"
+.text
+la	$s0, stringstore
+la	$s1, numstore
+.globl main
+main:
+la      $a0, prompt     #calling opening prompt
+li      $v0, 4
+syscall
+la $a0, str	#load address with my allocated space for string
+li $a1 65		# allocate space
+li $v0, 8		# user input string
+syscall
+la      $a0, newline    #newline
+syscall
+li      $t2, 0
+
+lb $t0, str($t2)
+sb $t0, ($s0) # load these characters
+addi $s0, $s0, 1 # load these characters
+bge $t0, 'A', checkZ #check if less than Z
+#j notVariable
+j main
+
+checkZ:
+ble $t0, 'Z', declareVar # start declaring variable
+
+j lowerCase # jump to see if variable name starts with lowercase
+
+lowerCase:
+bge $t0, 'a', checkz #check if less than z
+
+j main
+
+checkz:
+ble $t0, 'z', declareVar # start declaring variable
+
+j main
+
+declareVar:
+add $t2, $t2, 1
+lb $t0, str($t2) # load next character
+sb $t0, ($s0) # load these characters
+addi $s0, $s0, 1 
+beq $t0, ' ', endOfVarName
+beq $t0, $zero, main 
+
+j declareVar
+
+endOfVarName:
+add $t2, $t2, 1
+lb $t0, str($t2)
+beq $t0, ' ', endOfVarName
+beq $t0, '=', foundEqual
+beq $t0, $zero, notVariable 
+
+foundEqual:
+
+add $t2, $t2, 1
+lb $t0, str($t2)
+beq $t0, ' ', foundEqual
+bge $t0, '0', checkLessThanNine
+beq $t0, $zero, notVariable
+
+checkLessThanNine:
+ble $t0, '9' foundNumber
+
+j notVariable
+
+foundNumber:
+sb $t0, ($s1) # load these these numbers to address
+addi $s1, $s1, 1 # load these these numbers to address
+add $t2, $t2, 1
+lb $t0, str($t2)
+bge $t0, '0', checkLessThanNine
+
+j notVariable
+
+check:
+li $t0, ' '
+sb $t0, ($s1) # load these these numbers to address
+addi $s1, $s1, 1 # load these these numbers to address
+add $t2, $t2, 1
+la      $a0, newline    #newline
+syscall
+sb	$zero, ($s0) # just in case you want to see what is in the var in terms of the name
+la	$a0, stringstore
+li	$v0, 4
+syscall
+la      $a0, newline    #newline
+syscall
+sb	$zero, ($s1) # just in case you want to see what is in the var in terms of the name
+la	$a0, numstore
+li	$v0, 4
+syscall
+la      $a0, newline    #newline
+syscall
+j main
+
+notVariable:
+
+la      $a0, nv     #calling opening prompt
+li      $v0, 4
+syscall
+
+la      $a0, newline    #newline
+syscall
+
+j main
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
